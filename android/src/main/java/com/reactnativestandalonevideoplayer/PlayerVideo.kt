e51f292b3c38214333092af20a616e2fa90c2cf8
Weronika Grzeszczyk <werawera112@gmail.com>
2025-07-23 12:51:47 +0200

    prepare setVolume method

--- android/src/main/java/com/reactnativestandalonevideoplayer/PlayerVideo.kt
+++ android/src/main/java/com/reactnativestandalonevideoplayer/PlayerVideo.kt
@@ -117,6 +117,11 @@ class PlayerVideo(private val context: Context) {
     startProgressTimer()
   }
 
+  fun setVolume(volume: Float) {
+    Log.d("PlayerVideo", "setVolume")
+    player.setVolume(volume)
+  }
+
   fun play() {
     Log.d("PlayerVideo", "play")
     if (status == PlayerVideoStatus.finished) {
